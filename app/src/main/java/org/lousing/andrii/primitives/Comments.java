793818bd6c84d005f2882226ab21b68e509b4c14
neutral00 <to.arvmail@gmail.com>
2026-01-28 21:19:07 +0530

    tutorial on java comments and number system added

---
+++ app/src/main/java/org/lousing/andrii/primitives/Comments.java
@@ -0,0 +1,11 @@
+package org.lousing.andrii.primitives;
+
+public class Comments {
+
+  public static void showDemo() {
+    System.out.println("Comments in Java: ");
+    System.out.println("1. Single line comments. //");
+    System.out.println("2. Multi-line comments. /* */");
+    System.out.println("3. Document comments. /** */");
+  }
+}
---
+++ app/src/main/java/org/lousing/andrii/primitives/NumberSystem.java
@@ -0,0 +1,16 @@
+package org.lousing.andrii.primitives;
+
+public class NumberSystem {
+
+  public static void showDemo() {
+    int dec = 152;
+    int bin = 0b10011000;
+    int oct = 0230;
+    int hex = 0x98;
+
+    System.out.println(dec);
+    System.out.println(bin);
+    System.out.println(oct);
+    System.out.println(hex);
+  }
+}
